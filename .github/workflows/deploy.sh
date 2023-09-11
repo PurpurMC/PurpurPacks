6e213d87fb037cb4555f281c868563f09e30d4c3
granny <contact@granny.dev>
2023-09-10 23:40:21 -0700

    generate zipped datapacks

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-packs_json="packs.json"
-data=$(cat "$packs_json")
-cd "../"
-for pack in $(echo "$data" | jq -r '.packs | keys[]'); do
-    echo "STARTING ITERATION OVER PACK - PACK VARIABLES:"
-        index_number="$pack"
-        echo "PACK VARIABLES INFO: 'index_number' = $index_number"
-        pack_data=$(echo "$data" | jq -r ".packs[$pack]")
-        echo "PACK VARIABLES INFO: 'pack_data' = $pack_data"
-        directory_name=$(echo "$pack_data" | jq -r ".directory_name")
-        echo "PACK VARIABLES INFO: 'directory_name' = $directory_name"
-        version_number=$(echo "$pack_data" | jq -r ".version_number")
-        echo "PACK VARIABLES INFO: 'version_number' = $version_number"
+datapack_prefix="purpurpack_"
+datapack_folder="packs"
 
-        zip_filename="${directory_name}_${version_number}.zip"
-        echo "ZIP INFO: 'zip_filename' = $zip_filename"
-        cd "../"
-        cd "./packs/$directory_name" || exit  # Move into the directory
-        zip -r "../../latest_releases/$zip_filename" ./*  # Include only the contents of the directory
-        cd "../" || exit  # Move back to the original directory
+echo "Changed files: $ALL_CHANGED_FILES"
+
+for datapack_path in $datapack_folder/*; do
+    modrinth_file_path = "$datapack_path/modrinth.json"
+
+    # break if not a directory
+    if [ ! -d "$datapack_path" ]; then
+      break
+    fi
+
+    # break if datapack folder does not include modrinth.json
+    if [ ! -e modrinth_file_path ]; then
+      echo "No modrinth.json file located in \"$datapack_path\""
+      break
+    fi
+
+    # packs/<datapack> -> <datapack>
+    datapack_name = $datapack_path | cut -d'/' -f2
+
+    # combine modrinth.json with <datapack>/modrinth.json (datapack file overrides base file values)
+    modrinth_json = jq -s '.[0] * .[1]' modrinth.json modrinth_file_path
+
+    #create a zipped file inside packs/<datapack>/dist/ without including the modrinth.json file & dist/ directory
+    (cd $datapack_path && mkdir -p dist && zip -r "dist/${datapack_prefix}${datapack_name}_$(jq '.version_number' <<< modrinth_json).zip" . -x dist/ modrinth.json)
+
+    echo "Datapack located at: $(ls ${datapack_path}/dist)"
 done

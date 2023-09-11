9390d14673277274852077d2a2a00943c938113b
granny <contact@granny.dev>
2023-09-11 00:54:22 -0700

    add check for changed files

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,7 +3,7 @@
 datapack_prefix="purpurpack_"
 datapack_folder="packs"
 
-echo "Changed files: $ALL_CHANGED_FILES"
+base_modrinth_file_changed=$(jq 'map(select(startswith("modrinth.json")))|unique[]' <<< $ALL_CHANGED_FILES)
 
 for datapack_path in $datapack_folder/*; do
     modrinth_file_path="$datapack_path/modrinth.json"
@@ -21,6 +21,12 @@ for datapack_path in $datapack_folder/*; do
 
     # packs/<datapack> -> <datapack>
     datapack_name=$(cut -d'/' -f2 <<< $datapack_path)
+
+    # continue if base modrinth file was not changed and the changed files do not include the datapack name
+    if [ -z "$base_modrinth_file_changed" ] && [ -z $(jq "map(select(contains(${datapack_name})))|unique[]" <<< $ALL_CHANGED_FILES) ]; then
+        continue
+    fi
+
 
     # combine modrinth.json with <datapack>/modrinth.json (datapack file overrides base file values)
     modrinth_json=$(jq -s '.[0] * .[1]' modrinth.json $modrinth_file_path)
